a7941ca238d08d7d52ba46d82237f5f97e4168c2
rovica <54681116+rovica@users.noreply.github.com>
2019-09-11 15:10:01 -0300

    Update crawler_g1.py

--- crawler_g1.py
+++ crawler_g1.py
@@ -4,12 +4,12 @@ from bs4 import BeautifulSoup
 import requests
 
 #url coletada pelo robo
-url = str("http://g1.globo.com//g1.globo.com/busca/click?q=parceria+publico+privada&p=5&r=1568182739548&u=https%3A%2F%2Fg1.globo.com%2Fmg%2Ftriangulo-mineiro%2Fnoticia%2F2019%2F09%2F10%2Fconstrucao-de-usina-de-energia-solar-em-uberaba-e-tratada-em-audiencia-publica.ghtml&syn=False&key=8a6b7d75384cd8b809f50b1fd8365644")
+url = str("https://g1.globo.com/busca/?q=ppp&order=recent&page=1&from=now-1w")
 
 #correção da url duplicada
-print(url.replace('g1.globo.com//','',1 ))
-url = url.replace('g1.globo.com//','',1 )
-print(url)
+#print(url.replace('g1.globo.com//','',1 ))
+#url = url.replace('g1.globo.com//','',1 )
+#print(url)
 
 #request
 page = requests.get(url)
@@ -19,5 +19,5 @@ print(page.status_code)
 if page.status_code == 200:
 
     soup = BeautifulSoup(page.text, features="html.parser")
-    for noticia in soup.select("h1.content-head__title"):
+    for noticia in soup.select("div.widget--info__text-container"):
         print(noticia.text)
